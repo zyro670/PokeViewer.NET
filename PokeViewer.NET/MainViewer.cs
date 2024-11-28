82c0ef5ba49353cbcbd158b3658b9004db328501
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2024-11-28 15:38:14 -0700

    use InvariantCulture for Convert.ToDouble

--- PokeViewer.NET/MainViewer.cs
+++ PokeViewer.NET/MainViewer.cs
@@ -11,6 +11,7 @@ using PokeViewer.NET.SubForms;
 using PokeViewer.NET.WideViewForms;
 using SysBot.Base;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO.Compression;
 using System.Net.Sockets;
 using System.Reflection;
@@ -75,7 +76,7 @@ namespace PokeViewer.NET
             var sbb = await Executor.SwitchConnection.GetBotbaseVersion(token).ConfigureAwait(false);
             string replacement = Regex.Replace(sbb, @"\t|\n|\r", "");
             string vIn = replacement.Replace('"', ' ').Trim();
-            var vOut = Convert.ToDouble(vIn);
+            var vOut = Convert.ToDouble(vIn, CultureInfo.InvariantCulture);
             if (vOut < 2.4)
             {
                 DialogResult dialogResult = MessageBox.Show($"Current version of sysbot-base v{sbb.ToString().TrimEnd('\r', '\n')} does not match minimum required version. Download latest?", "An update is available", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
